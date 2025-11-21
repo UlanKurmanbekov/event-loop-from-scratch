fa873716a22a16efa0e87ece8c736f9b2bb2e831
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2025-11-22 04:58:35 +0600

    Clean up event loop code
    - Convert to class-based EventLoop
    - Add full type annotations
    - Use Enum for event types
    - Replace select with selectors
    - Extract IO waiting logic to separate method

--- event_loop.py
+++ event_loop.py
@@ -1,71 +1,96 @@
 import socket
-from select import select
+import selectors
 from collections import deque
-
-pending_tasks = deque()
-
-sockets_waiting_for_read = {}
-sockets_waiting_for_write = {}
+from typing import Generator
+from enum import Enum, auto
 
 
-def server():
+class EventType(Enum):
+    READ = auto()
+    WRITE = auto()
+
+
+def server(loop: 'EventLoop') -> Generator[tuple[EventType, socket.socket], None, None]:
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('localhost', 8000))
     server_socket.listen()
 
     while True:
-        yield 'read', server_socket
+        yield EventType.READ, server_socket
         client_socket, addr = server_socket.accept()
 
         print(f'Connection: {addr}')
-        pending_tasks.append(handle_client(client_socket))
+        loop.create_task(handle_client(client_socket))
 
 
-def handle_client(client_socket):
-    while True:
-        yield 'read', client_socket
-        request = client_socket.recv(4096)
+def handle_client(client_socket: socket.socket) -> Generator[tuple[EventType, socket.socket], None, None]:
+    try:
+        while True:
+            yield EventType.READ, client_socket
+            request = client_socket.recv(4096)
 
-        if not request:
-            break
-        else:
-            response = b'Hello World!\n'
+            if not request:
+                break
+            else:
+                response = b'Hello World!\n'
 
-            yield 'write', client_socket
-            client_socket.send(response)
-
-    client_socket.close()
-
-
-def event_loop():
-    while any([pending_tasks, sockets_waiting_for_read, sockets_waiting_for_write]):
-
-        if not pending_tasks:
-            ready_to_read, ready_to_write, _ = select(
-                sockets_waiting_for_read,
-                sockets_waiting_for_write,
-                []
-            )
-
-            for sock in ready_to_read:
-                pending_tasks.append(sockets_waiting_for_read.pop(sock))
-
-            for sock in ready_to_write:
-                pending_tasks.append(sockets_waiting_for_write.pop(sock))
-
-        try:
-            task = pending_tasks.popleft()
-
-            operation, sock = next(task)
-
-            if operation == 'read':
-                sockets_waiting_for_read[sock] = task
-            elif operation == 'write':
-                sockets_waiting_for_write[sock] = task
-        except StopIteration:
-            print('Done')
+                yield EventType.WRITE, client_socket
+                client_socket.send(response)
+    finally:
+        client_socket.close()
 
 
-pending_tasks.append(server())
-event_loop()
+class EventLoop:
+    def __init__(self) -> None:
+        self.selector = selectors.DefaultSelector()
+        self.pending_tasks = deque()
+        self._event_mapping = {
+            EventType.READ: selectors.EVENT_READ,
+            EventType.WRITE: selectors.EVENT_WRITE
+        }
+
+    def create_task(self, coro: Generator[tuple[EventType, socket.socket], None, None]) -> None:
+        self.pending_tasks.append(coro)
+
+    def _register_for_io(
+            self,
+            sock: socket.socket,
+            event_type: EventType,
+            task: Generator[tuple[EventType, socket.socket], None, None]
+    ) -> None:
+        self.selector.register(fileobj=sock, events=self._event_mapping[event_type], data=task)
+
+    def _wait_for_io(self) -> None:
+        events = self.selector.select()
+
+        for key, _ in events:
+            task = key.data
+            self.selector.unregister(key.fileobj)
+            self.create_task(task)
+
+    def run(self) -> None:
+        print('Event loop started...')
+
+        while True:
+            while self.pending_tasks:
+                try:
+                    task = self.pending_tasks.popleft()
+                    op, sock = next(task)
+
+                    if op == EventType.READ:
+                        self._register_for_io(sock, EventType.READ, task)
+                    elif op == EventType.WRITE:
+                        self._register_for_io(sock, EventType.WRITE, task)
+                except StopIteration:
+                    pass
+                except Exception as e:
+                    print(f"Task error: {e}")
+
+            self._wait_for_io()
+
+
+if __name__ == '__main__':
+    loop = EventLoop()
+    loop.create_task(server(loop))
+    loop.run()
